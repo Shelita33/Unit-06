0d7f5662668cbe8fa987d207d01773df6deb43ca
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-08-09 19:42:14 -0500

    priceCalculator.kt

---
+++ priceCalculator.kt
@@ -0,0 +1,11 @@
+import java.util.*
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+    print("Enter the wholesale cost and markup precentage: ")
+
+    // nextInt() reads the next integer. next() reads the String
+    var integer:Int = reader.nextInt()
+
+    println("You entered: $integer")
+ 
+}
